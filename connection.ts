e6bc5f600771ac6bfb02059c57d07f5e9d95be77
fares <fereshouidi298@gmail.com>
2025-08-01 00:48:46 +0100

    fixing

--- connection.ts
+++ connection.ts
@@ -1,26 +1,21 @@
 import mongoose from 'mongoose';
-import dotenv from "dotenv";
-
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-// if (!process.env.MONGODB_URI) {
-//     throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-// }
+const mongoConn = async () => {
+  if (!process.env.MONGODB_URI) {
+    console.error('Error: process.env.MONGODB_URI is undefined!');
+    throw new Error('MONGODB_URI is not defined in environment variables');
+  }
 
-
-const mongoConn = 
-
-    process.env.MONGODB_URI ?
-
-    mongoose.connect(process.env.MONGODB_URI)
-        .then(() => {
-            console.log('MongoDB connected');
-        })
-        .catch((err) => {
-            console.error('MongoDB connection error:', err);
-        })
-
-    : console.error('error : process.env.MONGODB_URI is undefined !')
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+    throw err;
+  }
+};
 
 export default mongoConn;
