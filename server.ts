10034edbcad342a9e8e59eb25c562542256c45d3
fares <fereshouidi298@gmail.com>
2025-08-01 00:17:43 +0100

    use serverless-http

--- server.ts
+++ server.ts
@@ -1,7 +1,5 @@
 import express from 'express';
 import mongoConn from './connection.js';
-// import cors from 'cors';
-// import { createDefaultMessages, updatePrimaryPrompt } from './controller/endpoint/message.js';
 import { MessageParams } from './types.js';
 import MessageRoute from './routes/messages.js'; 
 import ConversationRoute from './routes/conversation.js';
@@ -9,15 +7,14 @@ import UserRoute from './routes/user.js';
 import { primaryPrompt } from './constants/prompts.js';
 import dotenv from "dotenv";
 import { Server } from 'socket.io';
-// import io from './socket.io.js';
 import registerSocketHandlers from './sockets/server.js';
-// import { custimizeUserMessage, parseCustomizedMessage } from './constants/promptsComponnent/userMessage.js';
+import { Express } from 'express-serve-static-core';
+import serverless from 'serverless-http';
 
 dotenv.config();
 const io = new Server(3000)
 
 const app = express();
-// app.use(cors());
 app.use(express.json());
 
 app.use('/api', MessageRoute);
@@ -34,6 +31,8 @@ registerSocketHandlers(io);
 
 console.log({primaryPrompt: primaryPrompt.length});
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+export const handler = serverless(app);
+
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
