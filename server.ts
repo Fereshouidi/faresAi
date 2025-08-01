9ef0e40a983f7074c2031d151f4811910a230e02
fares <fereshouidi298@gmail.com>
2025-08-01 15:01:45 +0100

    fixing

--- server.ts
+++ server.ts
@@ -12,6 +12,7 @@ import registerSocketHandlers from './sockets/server.js';
 import { Express } from 'express-serve-static-core';
 import serverless from 'serverless-http';
 import http from 'http';
+import User from './models/user.js';
 
 dotenv.config();
 
@@ -35,6 +36,12 @@ const io = new Server(server, {
 app.get("/", (req, res) => {
   res.send("Server is working!");
   console.log("Server is working!");
+  
+});
+
+app.get("/getAllUsers", async (req, res) => {
+  const users = await User.find();
+  res.send(users)
   
 });
 
