bcf5708fc7b17c60fa40f4215f62a15457bca517
fares <fereshouidi298@gmail.com>
2025-08-01 01:33:22 +0100

    fixing

--- server.ts
+++ server.ts
@@ -11,18 +11,22 @@ import cors from 'cors';
 import registerSocketHandlers from './sockets/server.js';
 import { Express } from 'express-serve-static-core';
 import serverless from 'serverless-http';
+import http from 'http';
 
 console.log(1);
 
 
 dotenv.config();
-// const io = new Server(Number(process.env.PORT));
-
 console.log(2);
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server);
+
 
 
 app.get("/", (req, res) => {
